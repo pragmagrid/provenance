31a0b786faf676b545ea0171557c4a2f72d8aeb8
root <root@rocks-204.sdsc.edu>
2013-09-19 09:57:19 -0700

    add erlang to PATH

--- src/rocks-provenance/provenance.sh
+++ src/rocks-provenance/provenance.sh
@@ -1,6 +1,8 @@
 # provenance roll
+
 KARMA_BIN=/opt/provenance/karma-adaptor
 CYTOSCAPE_BIN=/opt/provenance/cytoscape/
+ERLANG_BIN=/opt/erlang/bin
 
-export PATH=$PATH:$KARMA_BIN:$CYTOSCAPE_BIN
+export PATH=$PATH:$KARMA_BIN:$CYTOSCAPE_BIN:$ERLANG_BIN
 export ADAPTOR_HOME=/opt/provenance/karma-adaptor
