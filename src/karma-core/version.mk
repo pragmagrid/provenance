07407f6e850ff82616c2b2170b333b2fa7d63599
root <root@rocks-204.sdsc.edu>
2013-09-24 11:22:18 -0700

    add karma server

---
+++ src/karma-core/version.mk
@@ -0,0 +1,10 @@
+PKGROOT		= /opt/provenance/karma
+NAME            = karma-service-core
+PKGNAME         = service-core
+VERSION         = v3.2.3
+
+RELEASE 	= 0
+TARBALL_POSTFIX	= tar.gz
+
+RPM.EXTRAS = %define __os_install_post %{nil}
+
