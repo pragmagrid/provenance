f8d84673e6d09c764a2f68f3ae27621f69ce72a9
root <root@rocks-204.sdsc.edu>
2013-09-25 10:17:34 -0700

    add karma messaging client

---
+++ src/karma-messaging/version.mk
@@ -0,0 +1,10 @@
+PKGROOT		= /opt/provenance/karma-client
+NAME            = karma-messaging-client
+PKGNAME         = client-core
+VERSION         = v3.2.3
+
+RELEASE 	= 0
+TARBALL_POSTFIX	= tar.gz
+
+RPM.EXTRAS = %define __os_install_post %{nil}
+
