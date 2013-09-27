b7b8d1e124f9838e836c4b67b9832667c2a90370
root <root@rocks-204.sdsc.edu>
2013-09-27 11:36:22 -0700

    delete karma-adaptor - was for specific project, no longer used

--- src/karma-adaptor/version.mk
+++
@@ -1,7 +0,0 @@
-PKGROOT	        = /opt/provenance/karma-adaptor
-NAME            = karma-adaptor
-VERSION         = v2.5
-RELEASE 	= 0
-TARBALL_POSTFIX	= tar.gz
-
-RPM.EXTRAS = %define __os_install_post %{nil}
