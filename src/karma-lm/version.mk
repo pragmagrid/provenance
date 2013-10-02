8b555030283de2dcd668ffc0ab13fe5d5ef82194
root <root@rocks-204.sdsc.edu>
2013-10-02 16:05:43 -0700

    initial per distriution as of 2013-10-01. Will need updates

---
+++ src/karma-lm/version.mk
@@ -0,0 +1,10 @@
+PKGROOT	        = /opt/provenance/karma-lm-adaptor
+KARMACLIENT	= /opt/provenance/karma-client
+LMVAR      	= /var/lm
+NAME            = karma-lm-adaptor
+PKGNAME         = LifeMapperKarma
+VERSION         = V2
+RELEASE 	= 0
+TARBALL_POSTFIX	= tar.gz
+
+RPM.EXTRAS = %define __os_install_post %{nil}
